5af9237820cf1704e9f84cae9cf4b0e51b13be72
Owen Chiu <q0972016679@gmail.com>
2019-04-22 22:07:48 +0900

    version 3_enter times shown

--- guessnumber.py
+++ guessnumber.py
@@ -1,13 +1,16 @@
 import random
 r = random.randint(1,100)
+count = 0
 while True:
+	count = count + 1
 	num = input('please input a number')
 	num = int(num)
 	if r == num:
 		print('your answer is correct')
+		print('this is your', count ,'time')
 		break
 	elif r > num:
 		print('bigger')
 	elif r < num:
 		print('smaller')
-		
+	print('you already tried',count, 'times')
